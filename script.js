60bfd39033373e453c6bca0d4420e233370e2172
Atif Pathan <atifkhan.p1@gmail.com>
2024-10-20 01:34:56 -0600

    Complete search bar functionality with proper focus and unfocused behavior as well as interaction with button and key press

--- script.js
+++ script.js
@@ -6,9 +6,10 @@ document.addEventListener('DOMContentLoaded', function() {
     const drop_btn_1 = document.querySelector(".btn-1");
     const drop_btn_2 = document.querySelector(".btn-2");
     const drop_btn_3 = document.querySelector(".btn-3");
-    const search_bar_input = document.querySelector(".search_bar_input");
+    let search_bar_input = document.querySelector(".search_bar_input");
+    const search_button = document.querySelector(".search-submit");
     let search_icon_image = document.getElementById("search-icon");
-    
+
     // dropdown_btn.addEventListener("click", function() {
     //     console.log(drop_menu.style.visibility);
     //     if (drop_menu.style.visibility === 'hidden') {
@@ -47,5 +48,25 @@ document.addEventListener('DOMContentLoaded', function() {
 
     search_bar_input.addEventListener("focus", function() {
         search_icon_image.setAttribute("src", "assets/icons/search_bold_white.svg");
+    })
+
+    search_bar_input.addEventListener("blur", function() {
+        search_icon_image.setAttribute("src", "assets/icons/search_bold_grey.svg");
+    })
+
+    search_bar_input.addEventListener("keydown", function(event) {
+        if (event.code === "Enter") {
+            setTimeout(() => {
+                search_bar_input.value = '';
+                search_bar_input.blur();
+            }, 200);
+        }
+    });
+
+    search_button.addEventListener("click", function() {
+        search_bar_input.value = '';
+        setTimeout(() => {
+            search_button.blur();
+        }, 100);
     })
 });
